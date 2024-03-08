0739a00c491f22c34c004875da1371831eb7a2a0
Morgan Peterman <mpeterma@redhat.com>
2024-03-07 23:45:47 -0500

    PEP8 plates.py

--- CS50p/2/plates.py
+++ CS50p/2/plates.py
@@ -5,17 +5,18 @@ def main():
     else:
         print("Invalid")
 
+
 def is_valid(input_text):
     punc = [" ", ".", "!", "?", ",", ":", ";"]
 
     # Check that the length is between 2 and 6
     if 2 <= len(input_text) <= 6:
         # Check that the first two char are letters
-        if input_text[0].isalpha() == True and input_text[1].isalpha() == True:
+        if input_text[0].isalpha() and input_text[1].isalpha():
             # Check that the first number isn't a 0
             i = 0
             while i < len(input_text):
-                if input_text[i].isalpha() == False:
+                if input_text[i].isalpha():
                     if input_text[i] == '0':
                         return False
                     else:
@@ -25,7 +26,7 @@ def is_valid(input_text):
             # Check if a digit comes before a letter
             x = 0
             while x < (len(input_text) - 1):
-                if input_text[x].isdigit() == True and input_text[x+1].isalpha() == True:
+                if input_text[x].isdigit() and input_text[x+1].isalpha():
                     return False
                 x += 1
 
@@ -37,5 +38,6 @@ def is_valid(input_text):
             return True
         else:
             return False
+
 
 main()
